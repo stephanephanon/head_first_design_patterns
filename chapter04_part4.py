40e0d6cebe8e0875e11126c640a36712d770f568
Stephanie Sydorko <stephanephanon@gmail.com>
2018-03-25 19:27:55 -0400

    reworks pizza store example to add gas station pizza

--- chapter04_part4.py
+++ chapter04_part4.py
@@ -158,9 +158,9 @@ class PizzaIngredientFactory(object):
 
 
 
-class NewYorkPizzaIngredientFactory(PizzaIngredientFactory):
+class EastCoastPizzaIngredientFactory(PizzaIngredientFactory):
     """
-    Factory for creating New-York style pizza ingredients
+    Factory for creating pizza ingredients on the east coast
     """
     def create_dough(self):
         """
@@ -199,9 +199,9 @@ class NewYorkPizzaIngredientFactory(PizzaIngredientFactory):
         return FreshClam()
 
 
-class ChicagoPizzaIngredientFactory(PizzaIngredientFactory):
+class MidwestPizzaIngredientFactory(PizzaIngredientFactory):
     """
-    Factory for creating New-York style pizza ingredients
+    Factory for creating pizza ingredients in the midwest
     """
     def create_dough(self):
         """
@@ -304,23 +304,40 @@ class PizzaFactoryBase(object):
 
 
 class NewYorkPizzaFactory(PizzaFactoryBase):
-    _ingredient_factory = NewYorkPizzaIngredientFactory
+    _ingredient_factory = EastCoastPizzaIngredientFactory
 
 
 class ChicagoPizzaFactory(PizzaFactoryBase):
-    _ingredient_factory = ChicagoPizzaIngredientFactory
+    _ingredient_factory = MidwestPizzaIngredientFactory
 
 
-class PizzaStoreBase(object):
+class PizzaStoreInterface(object):
     """
-    Represents a base pizza store.
-    Subclasses need to decide what kind of store they want to run
+    Represents a base pizza store behavior.
+    Implementers need to decide what kind of store they want to run
     """
-    _pizza_factory = None
-    _name = "Basic Style"
+    _name = "Pizza Store Interface"
+
+    def order_pizza(self, pizza_type):
+        """
+        Order a pizza of 'pizza type'
+        :param pizza_type: string
+        :return: Pizza object
+        """
+        raise NotImplementedError(
+            "Implementers must implement this method")
+
+
+class FreshPizzaStoreBase(PizzaStoreInterface):
+    """
+    Implements the pizza store interface to provide
+    fresh and tasty pizza
+    """
+    def __init__(self):
+        self.pizza_factory = None
 
     def create_pizza(self, pizza_type):
-        pizza_factory = self._pizza_factory()
+        pizza_factory = self.pizza_factory()
 
         pizza = None
         if pizza_type == 'cheese':
@@ -379,20 +396,62 @@ class PizzaStoreBase(object):
         return pizza
 
 
-class NewYorkCityPizzaStore(PizzaStoreBase):
+class FrozenPizzaStoreBase(PizzaStoreInterface):
+    """
+    Implements PizzaStoreInterface to provide a
+    microwaved frozen pizza
+    """
+    def create_pizza(self):
+        pizza = Pizza()
+        pizza.name = "Totino's Pizza"
+        pizza.dough = DoughBase()
+        pizza.sauce = TomatoSauceBase()
+        pizza.cheese = CheeseBase()
+        pizza.pepperoni = ChoppedPepperoni()
+        print("-- preparing a frozen {}".format(pizza.name))
+        return pizza
+
+    def microwave_pizza(self, pizza):
+        """
+        Warm up the pizza
+        """
+        print("-- microwave {} for 2 minutes".format(pizza.name))
+
+    def order_pizza(self, pizza_type):
+        """
+        Order the only type of pizza we sell --
+        a frozen cheese and cubed pepperoni pizza
+        """
+        pizza = None
+        if pizza_type == 'pepperoni':
+            pizza = self.create_pizza()
+            self.microwave_pizza(pizza)
+        else:
+            print("Unable to make your pizza. We don't sell it.")
+
+        return pizza
+
+
+class NewYorkCityPizzaStore(FreshPizzaStoreBase):
     """
     A new york city pizza store
     """
-    _pizza_factory = NewYorkPizzaFactory
     _name = 'New York Style'
 
+    def __init__(self):
+        super(NewYorkCityPizzaStore, self).__init__()
+        self.pizza_factory = NewYorkPizzaFactory
 
-class ChicagoPizzaStore(PizzaStoreBase):
+
+class ChicagoPizzaStore(FreshPizzaStoreBase):
     """
     A Chicago city pizza store
     """
-    _pizza_factory = ChicagoPizzaFactory
     _name = 'Chicago Style'
+
+    def __init__(self):
+        super(ChicagoPizzaStore, self).__init__()
+        self.pizza_factory = ChicagoPizzaFactory
 
     def bake_pizza(self, pizza):
         print("-- bake {} at 350F for 45 min".format(pizza.name))
@@ -401,20 +460,27 @@ class ChicagoPizzaStore(PizzaStoreBase):
         print("-- cutting {} the pizza into squares".format(pizza.name))
 
 
+class GasStationPizzaStore(FrozenPizzaStoreBase):
+    """
+    A gas station pizza store
+    """
+    _name = 'Chicago Style'
+
+
 if __name__ == '__main__':
     # let's open some pizza stores
     nyc_store = NewYorkCityPizzaStore()
-    chicago_store = ChicagoPizzaStore()
-
+    print("I went to New York and got some pizza")
     pizza = nyc_store.order_pizza('cheese')
-    print("I went to NYC and got a pizza: {}".format(pizza.name))
-
-    pizza = chicago_store.order_pizza('cheese')
-    print("I went to Chicago and got a pizza: {}".format(pizza.name))
-
-    # what about a nasty clam pizza
     pizza = nyc_store.order_pizza('clam')
-    print("I went to NYC and got a pizza: {}".format(pizza.name))
 
+    chicago_store = ChicagoPizzaStore()
+    print("I went to Chicago and got some pizza")
+    pizza = chicago_store.order_pizza('cheese')
     pizza = chicago_store.order_pizza('veggie')
-    print("I went to Chicago and got a pizza: {}".format(pizza.name))
+
+    # and some gas station pizza
+    gas_station = GasStationPizzaStore()
+    print("I went to the Gas Station and ordered some pizza")
+    pizza = gas_station.order_pizza('cheese')
+    pizza = gas_station.order_pizza('pepperoni')
